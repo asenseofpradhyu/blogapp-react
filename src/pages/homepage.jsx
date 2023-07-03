32074977e1ea4c3579cd0d801eaadfbac27428b0
Pradhumansinh Padhiyar <pradhu619@gmail.com>
2023-07-03 14:54:54 +0530

    [blog 2] --React router and header footer design

---
+++ src/pages/homepage.jsx
@@ -0,0 +1,5 @@
+function Homepage() {
+  return <div className="center-page">Home page</div>;
+}
+
+export default Homepage;
---
+++ src/pages/profile.jsx
@@ -0,0 +1,5 @@
+function Profile() {
+  return <div className="center-page">Profile Page</div>;
+}
+
+export default Profile;
